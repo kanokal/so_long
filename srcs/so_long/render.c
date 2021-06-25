6aa248723db376c1e20e8f887d99b5ba54b9d486
JunSik Pyo <vywnstlr@naver.com>
2021-06-25 20:06:29 +0900

    Update render.c

--- srcs/so_long/render.c
+++ srcs/so_long/render.c
@@ -32,50 +32,11 @@ static void		write_color(t_img_data *img, t_vec3 c)
 	img->color = (ir << 16) + (ig << 8) + ib;
 }
 
-static t_camera	*minirt_get_cur_cam(t_object *cam_list, int cam_nbr)
+int				sl_render(t_data *data, const char *line)
 {
-	int			idx;
-	t_object	*tmp;
-
-	idx = 0;
-	tmp = cam_list;
-	while (idx < cam_nbr)
+	while (*line != 0)
 	{
-		tmp = tmp->next;
-		idx++;
+		line++;
 	}
-	return ((t_camera *)tmp->element);
-}
-
-int				minirt_render(t_data *data, int bmp)
-{
-	int			i;
-	int			j;
-	double		u;
-	double		v;
-	t_camera	*cur_cam;
-	t_pixel		**pixel;
-	t_color		pixel_color;
-
-	cur_cam = minirt_get_cur_cam(data->scene.cam_list, data->cam);
-//	if (bmp == 1)
-//		pixel = minirt_create_pixel(data->scene.canvas.width, data->scene.canvas.height);
-	j = 0;
-	while (j < data->scene.canvas.height)
-	{
-		i = 0;
-		while (i < data->scene.canvas.width)
-		{
-			u = (double)i / (data->scene.canvas.width - 1);
-			v = (data->scene.canvas.height - (double)j - 1) / (data->scene.canvas.height - 1);
-			data->scene.ray = ray_primary(cur_cam, u, v);
-			pixel_color = ray_color(data->scene);
-			write_color(&data->img, pixel_color);
-			my_mlx_pixel_put(&data->img, i, j, data->img.color);
-			i++;
-		}
-		j++;
-	}
-	mlx_put_image_to_window(data->mlx, data->win, data->img.ptr, 0, 0);
 	return (0);
 }
