f4449387ac373fe7d6b54e2386b3ec1ba1121665
jpyo <jpyo@student.42.fr>
2021-06-26 17:13:31 +0900

    Testing

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,17 +6,76 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 17:05:51 by jpyo              #+#    #+#             */
-/*   Updated: 2021/06/25 17:06:33 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/06/26 16:26:32 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int main(void)
+int	ft_close(t_data *data)
 {
-	
+	mlx_destroy_window(data->mlx, data->win);
+	exit(0);
+	return (0);
 }
 
+int	press_key(int key, t_data *data)
+{
+	printf("Key in digit : %d\n", key);
+	if (key == KEY_ESC)
+		exit(0);
+	/*
+	else if (key == KEY_PAGE_UP)
+	{
+	}
+	else if (key == KEY_PAGE_DOWN)
+	{
+	}
+	else if (key == KEY_UP)
+	{
+	}
+	*/
+	return (0);
+}
+
+int	sl_loop(t_data *data)
+{
+	return (0);
+}
+
+int main(void)
+{
+	t_data		data;
+	t_img_data	img;
+	void		*img_ptr;
+	int			width = FONT_WIDTH * 14;
+	int			height = 20 * 5;
+
+	data.mlx = mlx_init();
+	data.win = mlx_new_window(data.mlx, 500, 500, "so_long");
+
+	//img.ptr = mlx_new_image(data.mlx, width, height);
+	img_ptr = mlx_xpm_file_to_image(data.mlx, "./images/wall.xpm", &width, &height);
+	//img.addr = mlx_get_data_addr(img.ptr, &img.bpp, &img.size, &img.endian);
+	//img.color = (255 << 16) + (255 << 8) + 255;
+	mlx_put_image_to_window(data.mlx, data.win, img_ptr, 10, 10);
+	data.img.ptr = mlx_xpm_file_to_image(data.mlx, "./images/dolphin.xpm", &width, &height);
+	mlx_put_image_to_window(data.mlx, data.win, data.img.ptr, 10, 10);
+	mlx_destroy_image(data.mlx, img_ptr);
+	//mlx_string_put(data.mlx, data.win, 0, 12, img.color, "1111111111111");
+	//mlx_string_put(data.mlx, data.win, 0, 32, img.color, "10010000000C1");
+	//mlx_string_put(data.mlx, data.win, 0, 52, img.color, "1000011111001");
+	//mlx_string_put(data.mlx, data.win, 0, 72, img.color, "1P0011E000001");
+	//mlx_string_put(data.mlx, data.win, 0, 92, img.color, "1111111111111");
+	//mlx_string_put(mlx, win, 0, 719, img.color, "double");
+	mlx_hook(data.win, DESTROYNOTIFY, 0, ft_close, &data);
+	mlx_hook(data.win, KEYPRESS, KEYPRESS_MASK, press_key, &data);
+	//mlx_loop_hook(data.mlx, sl_loop, &data);
+	mlx_loop(data.mlx);
+	return (0);
+}
+
+/*
 int	my_mlx_string_put(void *mlx_ptr, void *win_ptr, int x, int y, int color, char *string)
 {
 	static void *font = (void *)0;
@@ -57,3 +116,4 @@ int	my_mlx_string_put(void *mlx_ptr, void *win_ptr, int x, int y, int color, cha
 	}
 	return (0);
 }
+*/
