f3dc0d2dc055a5dc3a109fa157f7a8c37e811861
JunSik Pyo <vywnstlr@naver.com>
2021-06-29 17:37:52 +0900

    Move function

--- srcs/hook/sl_hook.c
+++ srcs/hook/sl_hook.c
@@ -12,45 +12,15 @@
 
 #include "../../includes/so_long.h"
 
-int	mouse_click(int button, int x, int y, t_data *data)
+/*
+int	sl_mouse_click(int button, int x, int y, t_data *data)
 {
 	printf("X and Y = (%d , %d)\n", x, y);
 	return (0);
 }
+*/
 
-int	press_key(int key, t_data *data)
-{
-	printf("Key in digit : %d\n", key);
-	if (key == KEY_ESC)
-		exit(0);
-	/*
-	else if (key == KEY_PAGE_UP)
-	{
-	}
-	else if (key == KEY_PAGE_DOWN)
-	{
-	}
-	else if (key == KEY_UP)
-	{
-	}
-	*/
-	return (0);
-}
-
-int	release_key(int key, t_data *data)
-{
-	if (key == KEY_PAGE_UP)
-	{
-		data->cam_previous = 1;
-	}
-	else if (key == KEY_PAGE_DOWN)
-	{
-		data->cam_next = 1;
-	}
-	return (0);
-}
-
-int	ft_close(t_data *data)
+int	sl_close(t_data *data)
 {
 	mlx_destroy_window(data->mlx, data->win);
 	exit(0);
