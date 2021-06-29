0a927a964419c5590f42c92868f1629e1c471afb
JunSik Pyo <vywnstlr@naver.com>
2021-06-29 17:40:11 +0900

    Add check function

--- srcs/utils/sl_utils.c
+++ srcs/utils/sl_utils.c
@@ -12,33 +12,90 @@
 
 #include "../../includes/so_long.h"
 
-void    sl_get_point(const char *s, t_so_long *sl)
+char	**sl_char_more_alloc(char **arr, int arr_size)
 {
-	if (*s == 0)
-		return ;
-	while (*s != 0)
+	int		idx;
+	int		tmp_size;
+	char	**tmp;
+
+	if (arr_size < 0)
+		arr_size = 0;
+	tmp = (char **)malloc(sizeof(char *) * (arr_size + 2));
+	if (tmp == NULL)
 	{
-		if (*s == 'E' && sl->sl_exit == 0)
-			sl->sl_exit = 1;
-		else if (*s == 'C' && sl->sl_collectible == 0)
-			sl->sl_collectible = 1;
-		else if (*s == 'P' && sl->sl_starting_position == 0)
-			sl->sl_starting_position = 1;
-		if (sl->sl_exit == 1 && sl->sl_collectible == 1 && sl->sl_starting_position == 1)
+		if (arr != NULL)
+			free(arr);
+		return (NULL);
+	}
+	tmp[arr_size + 1] = NULL;
+	tmp[arr_size] = NULL;
+	idx = 0;
+	while (idx < arr_size)
+		tmp[idx] = arr[idx];
+	if (arr != NULL)
+		free(arr);
+	return (tmp);
+}
+
+int		sl_check_points(t_so_long sl)
+{
+	if (sl.collectible < 1 || sl.exit_point < 1 || sl.starting_point < 1)
+		return (-1);
+	return (0);
+}
+
+void    sl_set_points(const char *line, t_so_long *sl)
+{
+	int	idx;
+
+	idx = 0;
+	while (line[idx] != 0)
+	{
+		if (line[idx] == 'E' && sl->exit_point == 0)
+			sl->exit_point = 1;
+		else if (line[idx] == 'C' && sl->collectible == 0)
+			sl->collectible = 1;
+		else if (line[idx] == 'P' && sl->starting_point == 0)
+		{
+			sl->starting_point = 1;
+			sl->py = sl->map.height;
+			sl->px = idx;
+		}
+		//else if (line[idx] == 'E' && sl->sl_starting_position == 0)
+		//	sl->enemy = 1;
+		if (sl->exit_point == 1 && sl->collectible == 1 && sl->starting_point == 1)
 			return ;
-		s++;
+		idx++;
 	}
 }
 
-int		sl_is_all_wall(const char *s)
+int		sl_is_ok_line(const char *line)
 {
-	if (*s == 0)
+	int	idx;
+
+	if (*line == 0)
 		return (0);
-	while (*s != 0)
+	idx = 0;
+	while (line[idx] != 0)
 	{
-		if (*s != '1')
+		if (idx == 0 && line[idx] != '1')
 			return (0);
-		s++;
+		idx++;
+	}
+	if (line[idx - 1] != '1')
+		return (0);
+	return (1);
+}
+
+int		sl_is_all_wall(const char *line)
+{
+	if (*line == 0)
+		return (0);
+	while (*line != 0)
+	{
+		if (*line != '1')
+			return (0);
+		line++;
 	}
 	return (1);
 }
