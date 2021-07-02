a0c8bb20a87a381f0688792d35cc29126d9fc0d2
jpyo <jpyo@student.42.fr>
2021-07-02 16:53:53 +0900

    Update hook

--- srcs/hook/sl_press_key.c
+++ srcs/hook/sl_press_key.c
@@ -6,7 +6,7 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:05:26 by jpyo              #+#    #+#             */
-/*   Updated: 2021/07/01 17:10:55 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/07/02 16:47:14 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,16 @@ static void	sl_player_move_up(t_sl_data *data)
 	}
 	else if (data->map.grid[data->player_data.py - 1][data->player_data.px] == 'E' && data->map.collectible == 0)
 		exit(0);
-	else if (data->map.grid[data->player_data.py + 1][data->player_data.px] == 'E')
+	else if (data->map.grid[data->player_data.py - 1][data->player_data.px] == 'E')
 	{
-		data->player_data.on_exit = 1;
 		data->map.grid[data->player_data.py - 1][data->player_data.px] = '0';
+		data->map.grid[data->player_data.py][data->player_data.px] = 'e';
 	}
 	ft_swap_char(&data->map.grid[data->player_data.py - 1][data->player_data.px], &data->map.grid[data->player_data.py][data->player_data.px]);
 	data->player_data.py--;
 	if (ft_bigint_add_val(data->player_data.mv_count, 1, 0) < 0)
 		ft_error_handling("Error\n");
-	data->render = 1;
+	sl_player_move(data);
 }
 
 static void	sl_player_move_down(t_sl_data *data)
@@ -48,14 +48,14 @@ static void	sl_player_move_down(t_sl_data *data)
 		exit(0);
 	else if (data->map.grid[data->player_data.py + 1][data->player_data.px] == 'E')
 	{
-		data->player_data.on_exit = 1;
 		data->map.grid[data->player_data.py + 1][data->player_data.px] = '0';
+		data->map.grid[data->player_data.py][data->player_data.px] = 'e';
 	}
 	ft_swap_char(&data->map.grid[data->player_data.py + 1][data->player_data.px], &data->map.grid[data->player_data.py][data->player_data.px]);
 	data->player_data.py++;
 	if (ft_bigint_add_val(data->player_data.mv_count, 1, 0) < 0)
 		ft_error_handling("Error\n");
-	data->render = 1;
+	sl_player_move(data);
 }
 
 static void	sl_player_move_left(t_sl_data *data)
@@ -71,14 +71,14 @@ static void	sl_player_move_left(t_sl_data *data)
 		exit(0);
 	else if (data->map.grid[data->player_data.py][data->player_data.px - 1] == 'E')
 	{
-		data->player_data.on_exit = 1;
 		data->map.grid[data->player_data.py][data->player_data.px - 1] = '0';
+		data->map.grid[data->player_data.py][data->player_data.px] = 'e';
 	}
 	ft_swap_char(&data->map.grid[data->player_data.py][data->player_data.px - 1], &data->map.grid[data->player_data.py][data->player_data.px]);
 	data->player_data.px--;
 	if (ft_bigint_add_val(data->player_data.mv_count, 1, 0) < 0)
 		ft_error_handling("Error\n");
-	data->render = 1;
+	sl_player_move(data);
 }
 
 static void	sl_player_move_right(t_sl_data *data)
@@ -94,30 +94,46 @@ static void	sl_player_move_right(t_sl_data *data)
 		exit(0);
 	else if (data->map.grid[data->player_data.py][data->player_data.px + 1] == 'E')
 	{
-		data->player_data.on_exit = 1;
 		data->map.grid[data->player_data.py][data->player_data.px + 1] = '0';
+		data->map.grid[data->player_data.py][data->player_data.px] = 'e';
 	}
 	ft_swap_char(&data->map.grid[data->player_data.py][data->player_data.px + 1], &data->map.grid[data->player_data.py][data->player_data.px]);
 	data->player_data.px++;
 	if (ft_bigint_add_val(data->player_data.mv_count, 1, 0) < 0)
 		ft_error_handling("Error\n");
-	data->render = 1;
+	sl_player_move(data);
 }
 
 int			sl_press_key(const int key, t_sl_data *data)
 {
 	printf("Key in digit : %d\n", key);
-	if (data->player_data.on_exit == 1)
+	if (data->map.grid[data->player_data.py][data->player_data.px] == 'e')
 		return (sl_press_key2(key, data));
 	if (key == KEY_ESC)
 		exit(0);
-	else if (key == KEY_W && data->map.grid[data->player_data.py - 1][data->player_data.px] != '1')
-		sl_player_move_up(data);
-	else if (key == KEY_S && data->map.grid[data->player_data.py + 1][data->player_data.px] != '1')
-		sl_player_move_down(data);
-	else if (key == KEY_A && data->map.grid[data->player_data.py][data->player_data.px - 1] != '1')
-		sl_player_move_left(data);
-	else if (key == KEY_D && data->map.grid[data->player_data.py][data->player_data.px + 1] != '1')
-		sl_player_move_right(data);
+	else if (key == KEY_W)
+	{
+		data->player_data.view_dir = VIEW_NORTH;
+		if (data->map.grid[data->player_data.py - 1][data->player_data.px] != '1')
+			sl_player_move_up(data);
+	}
+	else if (key == KEY_S)
+	{
+		data->player_data.view_dir = VIEW_SOUTH;
+		if (data->map.grid[data->player_data.py + 1][data->player_data.px] != '1')
+			sl_player_move_down(data);
+	}
+	else if (key == KEY_A)
+	{
+		data->player_data.view_dir = VIEW_WEST;
+		if (data->map.grid[data->player_data.py][data->player_data.px - 1] != '1')
+			sl_player_move_left(data);
+	}
+	else if (key == KEY_D)
+	{
+		data->player_data.view_dir = VIEW_EAST;
+		if (data->map.grid[data->player_data.py][data->player_data.px + 1] != '1')
+			sl_player_move_right(data);
+	}
 	return (0);
 }
--- srcs/hook/sl_press_key2.c
+++ srcs/hook/sl_press_key2.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sl_press_key2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/02 14:46:54 by jpyo              #+#    #+#             */
+/*   Updated: 2021/07/02 16:47:36 by jpyo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
@@ -8,12 +19,13 @@ static void	sl_player_move_up(t_sl_data *data)
 	if (data->map.grid[data->player_data.py - 1][data->player_data.px] == 'C')
 		data->map.collectible--;
 	data->map.grid[data->player_data.py - 1][data->player_data.px] = 'E';
+	data->map.grid[data->player_data.py][data->player_data.px] = 'P';
 	ft_swap_char(&data->map.grid[data->player_data.py - 1][data->player_data.px], &data->map.grid[data->player_data.py][data->player_data.px]);
-	data->player_data.on_exit = 0;
 	data->player_data.py--;
 	if (ft_bigint_add_val(data->player_data.mv_count, 1, 0) < 0)
 		ft_error_handling("Error\n");
-	data->render = 1;
+	data->player_data.view_dir = VIEW_NORTH;
+	sl_player_move(data);
 }
 
 static void	sl_player_move_down(t_sl_data *data)
@@ -23,12 +35,13 @@ static void	sl_player_move_down(t_sl_data *data)
 	else if (data->map.grid[data->player_data.py + 1][data->player_data.px] == 'C')
 		data->map.collectible--;
 	data->map.grid[data->player_data.py + 1][data->player_data.px] = 'E';
+	data->map.grid[data->player_data.py][data->player_data.px] = 'P';
 	ft_swap_char(&data->map.grid[data->player_data.py + 1][data->player_data.px], &data->map.grid[data->player_data.py][data->player_data.px]);
-	data->player_data.on_exit = 0;
 	data->player_data.py++;
 	if (ft_bigint_add_val(data->player_data.mv_count, 1, 0) < 0)
 		ft_error_handling("Error\n");
-	data->render = 1;
+	data->player_data.view_dir = VIEW_SOUTH;
+	sl_player_move(data);
 }
 
 static void	sl_player_move_left(t_sl_data *data)
@@ -38,12 +51,13 @@ static void	sl_player_move_left(t_sl_data *data)
 	else if (data->map.grid[data->player_data.py][data->player_data.px - 1] == 'C')
 		data->map.collectible--;
 	data->map.grid[data->player_data.py][data->player_data.px - 1] = 'E';
+	data->map.grid[data->player_data.py][data->player_data.px] = 'P';
 	ft_swap_char(&data->map.grid[data->player_data.py][data->player_data.px - 1], &data->map.grid[data->player_data.py][data->player_data.px]);
-	data->player_data.on_exit = 0;
 	data->player_data.px--;
 	if (ft_bigint_add_val(data->player_data.mv_count, 1, 0) < 0)
 		ft_error_handling("Error\n");
-	data->render = 1;
+	data->player_data.view_dir = VIEW_WEST;
+	sl_player_move(data);
 }
 
 static void	sl_player_move_right(t_sl_data *data)
@@ -53,26 +67,42 @@ static void	sl_player_move_right(t_sl_data *data)
 	else if (data->map.grid[data->player_data.py][data->player_data.px + 1] == 'C')
 		data->map.collectible--;
 	data->map.grid[data->player_data.py][data->player_data.px + 1] = 'E';
+	data->map.grid[data->player_data.py][data->player_data.px] = 'P';
 	ft_swap_char(&data->map.grid[data->player_data.py][data->player_data.px + 1], &data->map.grid[data->player_data.py][data->player_data.px]);
-	data->player_data.on_exit = 0;
 	data->player_data.px++;
 	if (ft_bigint_add_val(data->player_data.mv_count, 1, 0) < 0)
 		ft_error_handling("Error\n");
-	data->render = 1;
+	data->player_data.view_dir = VIEW_EAST;
+	sl_player_move(data);
 }
 
 int			sl_press_key2(const int key, t_sl_data *data)
 {
-	printf("Key in digit : %d\n", key);
 	if (key == KEY_ESC)
 		exit(0);
-	else if (key == KEY_W && data->map.grid[data->player_data.py - 1][data->player_data.px] != '1')
-		sl_player_move_up(data);
-	else if (key == KEY_S && data->map.grid[data->player_data.py + 1][data->player_data.px] != '1')
-		sl_player_move_down(data);
-	else if (key == KEY_A && data->map.grid[data->player_data.py][data->player_data.px - 1] != '1')
-		sl_player_move_left(data);
-	else if (key == KEY_D && data->map.grid[data->player_data.py][data->player_data.px + 1] != '1')
-		sl_player_move_right(data);
+	else if (key == KEY_W)
+	{
+		data->player_data.view_dir = VIEW_NORTH;
+		if (data->map.grid[data->player_data.py - 1][data->player_data.px] != '1')
+			sl_player_move_up(data);
+	}
+	else if (key == KEY_S)
+	{
+		data->player_data.view_dir = VIEW_SOUTH;
+		if (data->map.grid[data->player_data.py + 1][data->player_data.px] != '1')
+			sl_player_move_down(data);
+	}
+	else if (key == KEY_A)
+	{
+		data->player_data.view_dir = VIEW_WEST;
+		if (data->map.grid[data->player_data.py][data->player_data.px - 1] != '1')
+			sl_player_move_left(data);
+	}
+	else if (key == KEY_D)
+	{
+		data->player_data.view_dir = VIEW_EAST;
+		if (data->map.grid[data->player_data.py][data->player_data.px + 1] != '1')
+			sl_player_move_right(data);
+	}
 	return (0);
 }
