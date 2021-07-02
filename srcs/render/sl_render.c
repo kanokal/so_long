ff9b7fa5b9fab4028e3862f382784bc2e1a6a0f7
jpyo <jpyo@student.42.fr>
2021-07-02 16:53:05 +0900

    Move dir

--- srcs/render/sl_render.c
+++ srcs/render/sl_render.c
@@ -6,26 +6,27 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 17:28:28 by jpyo              #+#    #+#             */
-/*   Updated: 2021/07/01 19:50:38 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/07/02 15:19:56 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/so_long.h"
+#include "../../includes/so_long.h"
 
-// 몇번 움직였는지 표시
-
-static void	sl_render_texture(t_sl_data *data, int x, int y, int *texture)
+static void	sl_render_do(t_sl_data *data, int x, int y, int *texture)
 {
 	int		i;
 	int		j;
 
 	j = 0;
-	while (j < SPRITE_SIZE)
+	while (j < PIXEL_SIZE)
 	{
 		i = 0;
-		while (i < SPRITE_SIZE)
+		while (i < PIXEL_SIZE)
 		{
-			data->img.addr[(y * SPRITE_SIZE * data->canvas.width + j * data->canvas.width) + (x * SPRITE_SIZE + i)] = texture[j * SPRITE_SIZE + i];
+			if (texture[j * PIXEL_SIZE + i] != 0xff000000)
+				data->img.addr[y * PIXEL_SIZE * data->canvas.width + j * data->canvas.width + x * PIXEL_SIZE + i] = texture[j * PIXEL_SIZE + i];
+			else
+				data->img.addr[y * PIXEL_SIZE * data->canvas.width + j * data->canvas.width + x * PIXEL_SIZE + i] = data->background.addr[y * PIXEL_SIZE * data->canvas.width + j * data->canvas.width + x * PIXEL_SIZE + i];
 			i++;
 		}
 		j++;
@@ -44,17 +45,22 @@ void	sl_render(t_sl_data *data)
 		while (x < data->map.width)
 		{
 			if (data->map.grid[y][x] == '1')
-				sl_render_texture(data, x, y, data->texture.wall);
+				sl_render_do(data, x, y, data->texture.wall);
 			else if (data->map.grid[y][x] == '0')
-				sl_render_texture(data, x, y, data->texture.tile);
+				sl_render_do(data, x, y, data->texture.tile);
+			else if (data->map.grid[y][x] == 'e')
+			{
+				sl_render_do(data, x, y, data->texture.exit);
+				sl_render_do(data, x, y, data->texture.player);
+			}
 			else if (data->map.grid[y][x] == 'P')
-				sl_render_texture(data, x, y, data->texture.player);
+				sl_render_do(data, x, y, data->texture.player);
 			else if (data->map.grid[y][x] == 'C')
-				sl_render_texture(data, x, y, data->texture.collect);
+				sl_render_do(data, x, y, data->texture.collect);
 			else if (data->map.grid[y][x] == 'E')
-				sl_render_texture(data, x, y, data->texture.exit);
+				sl_render_do(data, x, y, data->texture.exit);
 			else if (data->map.grid[y][x] == 'W')
-				sl_render_texture(data, x, y, data->texture.enemy);
+				sl_render_do(data, x, y, data->texture.enemy);
 			else
 				ft_error_handling("Error\n");
 			x++;
