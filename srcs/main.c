6ca6ec42a76508d05a2b788c7a1f0637da387d7f
jpyo <jpyo@student.42.fr>
2021-07-02 16:53:13 +0900

    Testing

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:05:38 by jpyo              #+#    #+#             */
-/*   Updated: 2021/07/01 19:50:46 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/07/02 16:51:41 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,38 @@ int main(int argc, char *argv[])
 {
 	t_sl_data	data;
 
-	printf("%d\n", SPRITE_SIZE);
+	if (argc == 1)
+	{
+		sl_data_init(&data);
+		data.win = mlx_new_window(data.mlx, 1280, 1280, "so_long");
+		data.img.ptr = mlx_new_image(data.mlx, 1280, 1280);
+		data.img.addr = (int *)mlx_get_data_addr(data.img.ptr, &data.img.bpp, &data.img.size_line, &data.img.endian);
+		printf("bpp = %d / line = %d\n", data.img.bpp, data.img.size_line);
+		int		i;
+		int		j;
+
+		j = 0;
+		while (j < PIXEL_SIZE)
+		{
+			i = 0;
+			while (i < PIXEL_SIZE)
+			{
+				data.img.addr[j * data.img.size_line / 4 + i] = data.texture.player[j * PIXEL_SIZE + i];
+				i++;
+			}
+			j++;
+		}
+		mlx_put_image_to_window(data.mlx, data.win, data.img.ptr, 0, 0);
+		mlx_loop(data.mlx);
+	}
 	if (argc == 2)
 	{
 		sl_data_init(&data);
 		sl_parse(argv[1], &data);
 		if (sl_check_points(data.map) != 0)
 			ft_error_handling("Error\n");
-		data.canvas.width = data.map.width * SPRITE_SIZE;
-		data.canvas.height = data.map.height * SPRITE_SIZE;
+		data.canvas.width = data.map.width * PIXEL_SIZE;
+		data.canvas.height = data.map.height * PIXEL_SIZE;
 		data.win = mlx_new_window(data.mlx, data.canvas.width, data.canvas.height, "so_long");
 		data.img.ptr = mlx_new_image(data.mlx, data.canvas.width, data.canvas.height);
 		if (data.img.ptr == NULL)
