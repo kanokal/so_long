33c08356dfc3ed79bc256cf88c65ea4b8c131efb
jpyo <jpyo@student.42.fr>
2021-07-02 16:52:42 +0900

    Add size define

--- includes/sl_structures.h
+++ includes/sl_structures.h
@@ -6,77 +6,83 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 17:44:09 by jpyo              #+#    #+#             */
-/*   Updated: 2021/07/01 19:52:14 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/07/02 16:50:31 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SL_STRUCTURES_H
 # define SL_STRUCTURES_H
 
-# define IMG_W_SIZE 128
-# define IMG_H_SIZE 128
 # define SPRITE_SIZE 64
+# define PIXEL_SIZE 48
+# define ANIMATION_SPEED 20
 
-typedef struct	s_img_data
+# define VIEW_NORTH 0
+# define VIEW_SOUTH 1
+# define VIEW_WEST 2
+# define VIEW_EAST 3
+
+typedef struct		s_img_data
 {
-	void		*ptr;
-	int			*addr;
-	int			bpp;
-	int			size_line;
-	int			endian;
-	int			color;
-	int			width;
-	int			height;
-}				t_img_data;
+	void			*ptr;
+	int				*addr;
+	int				bpp;
+	int				size_line;
+	int				endian;
+	int				color;
+	int				width;
+	int				height;
+}					t_img_data;
 
-typedef struct	s_texture
+typedef struct		s_texture
 {
-	int			wall[SPRITE_SIZE * SPRITE_SIZE];
-	int			tile[SPRITE_SIZE * SPRITE_SIZE];
-	int			player[SPRITE_SIZE * SPRITE_SIZE];
-	int			collect[SPRITE_SIZE * SPRITE_SIZE];
-	int			exit[SPRITE_SIZE * SPRITE_SIZE];
-	int			enemy[SPRITE_SIZE * SPRITE_SIZE];
-}				t_texture;
+	int				wall[PIXEL_SIZE * PIXEL_SIZE];
+	int				tile[PIXEL_SIZE * PIXEL_SIZE];
+	int				player[PIXEL_SIZE * PIXEL_SIZE];
+	int				collect[PIXEL_SIZE * PIXEL_SIZE];
+	int				exit[PIXEL_SIZE * PIXEL_SIZE];
+	int				enemy[PIXEL_SIZE * PIXEL_SIZE];
+}					t_texture;
 
-typedef struct	s_canvas
+typedef struct		s_canvas
+	{
+	int				width;
+	int				height;
+}					t_canvas;
+
+typedef struct		s_sl_map
 {
-	int			width;
-	int			height;
-}				t_canvas;
+	char			**grid;
+	int				width;
+	int				height;
+	short			exit_point;
+	short			collectible;
+	short			starting_point;
+	short			enemy;
+}					t_sl_map;
 
-typedef struct	s_sl_map
+typedef struct		s_player_data
 {
-	char		**grid;
-	int			width;
-	int			height;
-	short		exit_point;
-	short		collectible;
-	short		starting_point;
-	short		enemy;
-}				t_sl_map;
+	int				px;
+	int				py;
+	char			view_dir;
+	char			rev;
+	int				animation;
+	t_bigint		*mv_count;
+}					t_player_data;
 
-typedef struct	s_player_data
+typedef struct		s_sl_data
 {
-	int			px;
-	int			py;
-	char		on_exit;
-	t_bigint	*mv_count;
-}				t_player_data;
-
-
-typedef struct	s_sl_data
-{
-	void		*mlx;
-	void		*win;
-	t_canvas	canvas;
-	t_img_data	img;
-	t_img_data	background;
-	t_img_data	move;
-	t_sl_map	map;
-	t_texture	texture;
-	char		render;
+	double			ratio;
+	void			*mlx;
+	void			*win;
+	t_canvas		canvas;
+	t_img_data		img;
+	t_img_data		background;
+	t_img_data		move;
+	t_sl_map		map;
+	t_texture		texture;
 	t_player_data	player_data;
-}				t_sl_data;
+}					t_sl_data;
 
 #endif
