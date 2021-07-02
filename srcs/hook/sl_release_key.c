b5f2024ed6ed7a086baf1865b3199743edccdacb
jpyo <jpyo@student.42.fr>
2021-07-02 16:54:06 +0900

    Maybe unused

--- srcs/hook/sl_release_key.c
+++ srcs/hook/sl_release_key.c
@@ -6,7 +6,7 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:05:22 by jpyo              #+#    #+#             */
-/*   Updated: 2021/06/30 18:24:00 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/07/02 14:37:21 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 int	sl_release_key(const int key, t_sl_data *data)
 {
-	if (key == KEY_W && data->render == 1)
-	{
-		data->render = 0;
-	}
-	else if (key == KEY_S && data->render == 1)
-	{
-		data->render = 0;
-	}
-	else if (key == KEY_A && data->render == 1)
-	{
-		data->render = 0;
-	}
-	else if (key == KEY_D && data->render == 1)
-	{
-		data->render = 0;
-	}
+	// if (key == KEY_W && data->render == 1)
+	// {
+	// 	data->render = 0;
+	// }
+	// else if (key == KEY_S && data->render == 1)
+	// {
+	// 	data->render = 0;
+	// }
+	// else if (key == KEY_A && data->render == 1)
+	// {
+	// 	data->render = 0;
+	// }
+	// else if (key == KEY_D && data->render == 1)
+	// {
+	// 	data->render = 0;
+	// }
 	return (0);
 }
